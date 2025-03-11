7c17cedfc2977780445b0a1a13f471069487e0a9
Si0dot7 <felm27010@gmail.com>
2025-03-11 23:29:16 +0700

    fix button loading

--- src/pages/Loading.jsx
+++ src/pages/Loading.jsx
@@ -1,48 +1,49 @@
 import { useNavigate } from "react-router-dom";
-import "./LoadPic.css"
+import "./LoadPic.css";
 import React from "react";
 
 function Loading() {
+  const navigate = useNavigate();
 
-    const navigate = useNavigate();
+  return (
+    <div className="flex flex-col items-center h-screen p-4 ">
+      <img
+        src="/newShirt.jpg"
+        alt="loadingPic"
+        className="w-auto h-full m-12 flex justify-center items-center shadow-2xl"
+      />
 
-    return (
+      <div className="font-[Inter] font-semibold text-3xl sm:text-[38px] text-center m-4 mt-5">
+        The Future of Sharing
+        <br />
+        Borrow What You Need
+        <br />
+        Lend What You Don't
+      </div>
 
-        <div className="flex flex-col items-center h-screen p-4 ">
+      <div className="text-gray-400 text-sm text-center">
+        Less buying, more sharing——smart lending
+        <br />
+        starts here
+      </div>
 
-            <img 
-                src="/newShirt.jpg" 
-                alt="loadingPic" 
-                className="w-auto h-full m-12 flex justify-center items-center shadow-2xl" />
-                
-            <div className="font-[Inter] font-semibold text-3xl sm:text-[38px] text-center m-4 mt-5">
-                The Future of Sharing<br/>
-                Borrow What You Need<br/>
-                Lend What You Don't
-            </div>
+      <div className="flex flex-col items-center">
+        <button
+          onClick={() => navigate("/Login")}
+          className="w-[100%] p-4  mt-10 bg-blue-600 text-white rounded-md hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-blue-500 cursor-pointer"
+        >
+          Get Start
+        </button>
 
-            <div className="text-gray-400 text-sm text-center">
-                Less buying, more sharing——smart lending<br/> 
-                starts here
-            </div>
-
-            <button
-                onClick={() => navigate("/Login")}
-                className="w-[50%] p-4 mt-10 bg-blue-600 text-white rounded-md hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-blue-500 cursor-pointer"
-            >
-                Get Start
-            </button>
-
-            <button
-                onClick={() => navigate("/Register")}
-                className="w-[50%] p-4 mt-4 mb-4 bg-gray-200 text-gray-700 rounded-md hover:bg-gray-300 focus:outline-none focus:ring-2 focus:ring-gray-100 cursor-pointer"
-            >
-                I'm new, Let's Register
-            </button>
-            
-        </div>
-        
-    )
+        <button
+          onClick={() => navigate("/Register")}
+          className="w-[100%] p-4 mt-4 mb-4 bg-gray-200 text-gray-700 rounded-md hover:bg-gray-300 focus:outline-none focus:ring-2 focus:ring-gray-100 cursor-pointer"
+        >
+          I'm new, Let's Register
+        </button>
+      </div>
+    </div>
+  );
 }
 
 export default Loading;
